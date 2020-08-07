53855537dd74f68e77f281537a654fc8288db337
JonesTPG <joonas.ryynanen1@gmail.com>
2020-08-07 09:27:02 +0300

    1.05 done.

--- part1/project/index.js
+++ part1/project/index.js
@@ -3,4 +3,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("server responding");
+});
+
 app.listen(port, () => console.log("Server started in port " + port));
