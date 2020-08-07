abe97d464155f9718bb30f9de5d812e8ced8d43f
JonesTPG <joonas.ryynanen1@gmail.com>
2020-08-07 10:54:53 +0300

    1.07 tested and done.

--- part1/main_application/index.js
+++ part1/main_application/index.js
@@ -1,5 +1,11 @@
+const express = require("express");
 const { v4 } = require("uuid");
 const timestamp = require("time-stamp");
+
+const app = express();
+const port = 3000;
+
+let string;
 
 const generateUuid = () => {
   return v4();
@@ -10,8 +16,16 @@ const generateTimeStamp = () => {
 };
 
 const randomString = () => {
-  console.log(generateTimeStamp() + ": " + generateUuid());
+  let newString = generateTimeStamp() + ": " + generateUuid();
+  console.log(newString);
+  string = newString;
   setTimeout(randomString, 5000);
 };
+
+app.get("/", (req, res) => {
+  res.send(string);
+});
+
+app.listen(port, () => console.log("Server started in port " + port));
 
 randomString();
