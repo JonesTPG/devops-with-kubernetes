5736fcd498f7f336f9bb63bf818fe51e60d09199
JonesTPG <joonas.ryynanen1@gmail.com>
2020-08-07 11:25:47 +0300

    created dockerignore files and start exercise 1.09. routing request doesn't work yet.

--- part1/second-application/index.js
+++ part1/second-application/index.js
@@ -3,10 +3,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let counter = 0;
+
 app.get("/", (req, res) => {
-  res.send(string);
+  counter = counter + 1;
+  res.send("pong" + counter);
 });
 
 app.listen(port, () => console.log("Server started in port " + port));
-
-randomString();
