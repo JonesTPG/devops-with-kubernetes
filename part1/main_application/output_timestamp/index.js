8fb3e9cd924befc73bddc948959c0cee9e898cd5
JonesTPG <joonas.ryynanen1@gmail.com>
2020-08-16 10:15:59 +0300

    bugfix to timestamp outputting.

--- part1/main_application/output_timestamp/index.js
+++ part1/main_application/output_timestamp/index.js
@@ -5,11 +5,12 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  fs.readFile('/usr/src/app/files/file.txt', function (err, contents) {
+  fs.readFile('/usr/src/app/files/file.txt', 'utf-8', function (err, contents) {
     if (err) {
       console.log('error reading file');
       res.status(500).send();
     } else {
+      console.log(contents);
       res.send(contents);
     }
   });
